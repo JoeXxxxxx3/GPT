9a8ba535fcbb83fcb5986db335da910c0c2f43b5
JoeXxxxxx3 <416261728@qq.com>
2023-11-06 22:24:35 +0800

    [md commite] update doc title

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -145,10 +145,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          Psychological Chatbot
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Enrich your life today.Yesterday is history, and tomorrow is mystery..
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
